4a03431ffb686461a9dce1574b7fc3e9818f7533
FilipMichalsky <filip_michalsky@g.harvard.edu>
2018-04-04 16:23:22 -0400

    t2 t3 t4

--- T4/problem1.py
+++ T4/problem1.py
@@ -10,6 +10,28 @@ from copy import copy
 # an equivalent numpy function or writing your own function). The program
 # will then output how the points merge at each step and the distance at which
 # they merge.
+def plot_unit_dist (norm, norm_name, min_frac=0.1):
+    #Plot 2D vectors with norm <= 1.
+    # https://glowingpython.blogspot.com/2011/04/plotting-p-norm-unit-circles
+    #    -with.html
+    
+    N_SAMPLES = 5000
+    points_used = 0  
+    f = plt.figure()
+    for _ in tqdm(range(N_SAMPLES)):
+        # get a random vector.
+        x = np.array([np.random.rand()*2 - 1, np.random.rand()*2 - 1])
+        # check if point is in unit distance 
+        if norm(x)<1:
+            points_used += 1
+            plt.plot(x[0], x[1], 'bo')
+
+    frac_used = points_used/N_SAMPLES
+    plt.axis([-1.2, 1.2, -1.2, 1.2])
+    plt.xlabel('$x_1$', size=16)
+    plt.ylabel('$x_2$', size=16)
+    plt.title('{}'.format(norm_name), size=18)
+    plt.show()
 
 def dist_min(cluster_1,cluster_2,norm):
 	# Calculate the minimum distance between the two clusters. 
@@ -24,75 +46,152 @@ def dist_min(cluster_1,cluster_2,norm):
 
 def dist_max(cluster_1,cluster_2,norm):
 	# Calculate the maximum distance between the two clusters.
-	return 0
+	max_dist = np.float('-inf')
+	for p1 in cluster_1:
+		for p2 in cluster_2:
+			n_dist = norm(p1 - p2)
+			if n_dist>max_dist:
+				max_dist = n_dist
+	return max_dist
 
 def dist_centroid(cluster_1,cluster_2,norm):
 	# Calculate the centroid distance between the two clusters.
-	return 0
+	centroid_1 = np.mean(cluster_1, axis=0)
+	centroid_2 = np.mean(cluster_2, axis=0)
+	return norm(centroid_1 - centroid_2)
 
 def dist_avg(cluster_1, cluster_2,norm):
 	# Calculate the geometric average distance between the two clusters.
-	return 0
+    G_G_prime = cluster_1.shape[0]*cluster_1.shape[1]
+    total_dist = 0
+    for point1 in cluster_1:
+        for point2 in cluster_2:
+            total_dist += norm(point1 - point2)
+    return total_dist/G_G_prime
 
-# Example code below does the L2 norm for min distance.
+# l1_norm taken from numpy
+l1_norm = lambda x:np.linalg.norm(x, ord=1)
 
-# List of norms (this currently only has l2 norm)
-norms_list = [np.linalg.norm]
-norm_names = ["l2 norm"]
+# inf_norm taken from numpy
 
-# List of distances (this currently only has dist_min)
-dist_list = [dist_min]
-dist_names = ["min dist"]
+inf_norm = lambda x:np.linalg.norm(x, ord=np.inf)
+
+norm_names = ["l2 norm", 'l1 norm', 'inf norm']
+norms_list = [np.linalg.norm, l1_norm, inf_norm]
+
+dist_names = ["min dist", 'max dist', 'avg dist', 'centroid dist']
+dist_list = [dist_min, dist_max, dist_avg, dist_centroid]
 
 #########################################################################
 # You should not have to change anything below this line. However, feel free
 # to do whatever you find most convenient.
 
-for norm_i in range(len(norms_list)):
-	for dist_i in range(len(dist_list)):
-		print("Using distance function " + str(dist_names[dist_i]) + 
-			"and using norm " + str(norm_names[norm_i]))
+def find_which_merge ():
+    # Store which two clusters are the first to merge.
+    df_clusters_merged = pd.DataFrame(columns=['norm', 'dist', 'clust1', 'clust2'])
 
-		#Initialize clusters, points, and step
-		# Define our points and initial clusters
-		points = np.array([[[0.1,0.5]],[[0.35,0.75]],[[0.28,1.35]],[[0,1.01]]])
-		cluster_1 = np.concatenate((points[0],points[1]))
-		cluster_2 = points[2]; cluster_3 = points[3]
-		clusters = [cluster_1,cluster_2,cluster_3]
-		step = 1
+    for norm_i in range(len(norms_list)):
+        for dist_i in range(len(dist_list)):
+            print("Using distance function " + str(dist_names[dist_i]) +
+                  " and using norm " + str(norm_names[norm_i]))
 
-		# Continue until there is only one cluster left in our list of clusters
-		while len(clusters)>1:
-			print("----------------------------------")
-			print("Clusters at step " + str(step))
-			for clust in clusters:
-				print(list(clust))
-			step += 1
-			min_dist = np.inf
-			merge_i = -1
-			merge_j = -1
-			# Iterate through all the possible cluster pairs and test distance
-			for i in range(len(clusters)):
-				for j in range(len(clusters)):
-					# If statement ensure i=j will not be tested
-					if i<j:
-						n_dist = dist_list[dist_i](
-							clusters[i],clusters[j],norms_list[norm_i])
-						if n_dist < min_dist:
-							# Record minimum distance and indices
-							min_dist = n_dist
-							merge_i = i
-							merge_j = j
-			# Merge together clusters with the minimum distance
-			clusters[merge_i] = np.concatenate((clusters[merge_i],
-				clusters[merge_j]),axis=0)
-			del clusters[merge_j]
-			print("Most recent merge distance " + str(min_dist))
+            # Initialize clusters, points, and step.
+            # Define our points and initial clusters.
+            points = np.array(
+                  [[[0.1, 0.5]], [[0.35, 0.75]], [[0.28, 1.35]], [[0, 1.01]]])
+            cluster_1 = np.concatenate((points[0], points[1]))
+            cluster_2 = points[2]
+            cluster_3 = points[3]
+            clusters = [cluster_1, cluster_2, cluster_3]
+            step = 1
 
-		print("----------------------------------")
-		print("Clusters at step" + str(step))
-		for clust in clusters:
-			print(list(clust))
+            # Continue until there is only one cluster left in our list of
+            # clusters
+            while len(clusters)>1:
+                # Display current clusters.
+                print("----------------------------------")
+                print("Clusters at step {}:".format(step))
+                for q, clust in enumerate(clusters):
+                    print('Cluster {}:'.format(q + 1))
+                    for p in clust:
+                        print('\t{}'.format(p))
+
+                step += 1
+                min_dist = np.inf
+                merge_i = -1
+                merge_j = -1
+                # Iterate through all the possible cluster pairs and test
+                # distance.
+                # Iterate through all the possible cluster pairs and test distance
+                for i in range(len(clusters)):
+                    for j in range(len(clusters)):
+                        # If statement ensure i=j will not be tested
+                        if i<j:
+                            n_dist = dist_list[dist_i](
+                                clusters[i],clusters[j],norms_list[norm_i])
+                            if n_dist < min_dist:
+                                # Record minimum distance and indices
+                                min_dist = n_dist
+                                merge_i = i
+                                merge_j = j
+                ##########
+                # Record which two clusters are first to merge.
+                ##########
+                if step==2: 
+                    new_row = {'norm':str(norm_names[norm_i]),
+                               'dist':str(dist_names[dist_i]),
+                               'clust1':merge_i + 1,
+                               'clust2':merge_j + 1}
+                    df_clusters_merged = df_clusters_merged.append(new_row,
+                                                           ignore_index=True)
+
+                # Merge together clusters with the minimum distance.
+                print('Merging clusters:')
+                print('{}: {}\t'.format(merge_i + 1, clusters[merge_i]))
+                print('{}: {}\t'.format(merge_j + 1, clusters[merge_j]))
+
+                clusters[merge_i] = np.concatenate((clusters[merge_i],
+                                                    clusters[merge_j]), axis=0)
+                del clusters[merge_j]
+                print("Most recent merge distance " + str(min_dist))
+
+            print("----------------------------------")
+            print("Clusters at step" + str(step))
+            for clust in clusters:
+                print(list(clust))
+    print("----------------------------------\n")
+    return df_clusters_merged
+
+
+if __name__ == '__main__':
+	import sys
+	import sklearn.cluster as cluster
+	from tqdm import tqdm
+	from numpy import ndarray
+	import matplotlib.pyplot as plt
+	import seaborn as sns
+
+	import numpy as np
+	import pandas as pd
+
+	for i,norm in enumerate(norms_list):
+		names = ['l2_norm', 'l1_norm', 'inf_norm']
+		plot_unit_dist(norm,names[i])
+	a = find_which_merge()
+	print(a.iloc[:,:])
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- T4/problem3.py
+++ T4/problem3.py
@@ -1,49 +1,279 @@
 # CS 181, Spring 2017
 # Homework 4: Clustering
-# Name:
-# Email:
+# Name: Filip Michalsky
+# Email: filip_michalsky@g.harvard.edu
 
-import numpy as np 
 import matplotlib.pyplot as plt
+from numpy import ndarray
+import numpy as np
+import pandas as pd
+import sklearn.cluster as cluster
+import sys
+from tqdm import tqdm
+import math
 import matplotlib.image as mpimg
 
 class KMeans(object):
-	# K is the K in KMeans
-        def __init__(self, K):
-		self.K = K
+    
+    # K-Means clustering implementation
 
-	# X is a (N x 28 x 28) array where 28x28 is the dimensions of each of the N images.
-	def fit(self, X):
-		pass
+    def __init__ (self, K, rand_seed=0,
+                  max_iter=80, threshold=1e-4):
+        
+        self.K = K
+        self.centroids = None
+        self.clusters = None
+        self.distortion = None
+        np.random.seed(rand_seed)
+        self.max_iter = max_iter
+        self.X = None
+        self.thresh = threshold
+        self.prev_centroids = None
+        
+    def fit (self, X):
+        self.X = X
+        # initialize clusters as zeros of length of shape 
+        self.clusters = np.zeros(X.shape[0], dtype=np.uint8)
+        
+        # initialize  K centroids randomly
+        select_idx = np.random.choice(list(range(X.shape[0])), size=self.K, replace=False)
+        self.centroids = X[select_idx, :]
+        self.distortion = []  # cast to ndarray when finished
+        
+        for j in tqdm(range(self.max_iter)):
+            
+            # Assign each observation to closest cluster.
+            for i, x_i in enumerate(X):
+                distances = self._get_dist_from_centroids(x_i)
+                # get the index of the cluster with the minimal euclidean distance
+                # of the centroid to the point
+                self.clusters[i] = np.argmin(distances)
+                
+                
+            # append the amount of error after each iteration
+            self.distortion.append(self._clusters_error(X))
+            
+            # now, after looping through all the points, update cluster assignments
+            self._new_centroids(X)
+            
+            
+            
+                
+            update_val = self.centroids - self.prev_centroids
+            if np.linalg.norm(update_val)**2 < self.thresh:
+                print('K-Means converged after {} iterations.'.format(j))
+                self.distortion = np.array(self.distortion)
+                return
+            #sys.stdout.flush()
+        
+        if np.linalg.norm(center_shift)**2 > self.thresh:
+             print('The KMeans algorithm has not converged after {} iterations!'.format(self.max_iter))
+        self.distortion = np.array(self.distortion)
 
-	# This should return the arrays for K images. Each image should represent the mean of each of the fitted clusters.
-	def get_mean_images(self):
-		pass
+    
+    def _get_dist_from_centroids(self, x_i):
+        # calculate a vector of distances for a data point x_i from each centroid
+        
+        distances = []
+        
+        for c in self.centroids:
+            distances.append(np.linalg.norm(x_i-c))
+        return distances
 
-	# This should return the arrays for D images from each cluster that are representative of the clusters.
-	def get_representative_images(self, D):
-		pass
+    def _new_centroids (self, X):
+        # Update centroids for the average feature vector belonging to each clusters
+        self.prev_centroids = self.centroids.copy()
 
-	# img_array should be a 2D (square) numpy array.
-	# Note, you are welcome to change this function (including its arguments and return values) to suit your needs. 
-	# However, we do ask that any images in your writeup be grayscale images, just as in this example.
-	def create_image_from_array(self, img_array):
-		plt.figure()
-		plt.imshow(img_array, cmap='Greys_r')
-		plt.show()
-		return
+        for k in range(self.K):
+            # Get the mean vector over points in each cluster.
+            cluster_data = [X[i] for i in range(X.shape[0]) if self.clusters[i]==k]
+            self.centroids[k] = np.mean(cluster_data, axis=0)
+        
+    def _clusters_error (self, X):
+        # Calculates the distortion which is 
+        # equivalent to the within-cluster sum-of-squared-errors (SSE) 
+        #for all points from the centroids for all clusters.
+        
+        err = 0
+        for i, x_i in enumerate(X):
+            cluster = self.clusters[i]
+            centroid = self.centroids[cluster]
+            err += np.linalg.norm(x_i-centroid) ** 2
+        return err
+    
+    def get_distortion(self):
+        return self.distortion[-1]
+    
+    def get_representative_images (self, J,as_dict=False):
+        
+        # get couple 'best' representative images
+        # meaning the first J closest in distance to the centroid image
+        
+        # Credit goes to Nate Stein for helping me a lot with the plotting
+        
+        result = {}
+        
+        # iterate through clusters
+        for k in range(self.K):
+            cluster_data = self.X[np.where(self.clusters == k)]
+            # Check whether you have some empty clusters:
+            if len(cluster_data) == 0:
+                result[k] = None
+                continue
+                
+            # Get distance for each point from the centroid.
+            distances = np.zeros(cluster_data.shape[0], dtype=float)
+            for i, x_i in enumerate(cluster_data):
+                distances[i] = np.linalg.norm(self.centroids[k]- x_i)
+                
+            # Get points with smallest distance to centroid using argsort 
+            # which returns indices corresponding as if the array was sorted.
+            smallest_idx = distances.argsort()[:J]
+            
+            result[k] = self.X[smallest_idx]
+            
+        if as_dict:
+            return result
+
+        result_list = []
+        for k in range(self.K):
+            for img in result[k]:
+                result_list.append(img)
+        return result_list
+    
+
+
+
+    def get_plots(self, img_arrays, ncols=3, show_img=False,
+                   save_path=None, fig_title=None, plt_titles=None):
+    	# Credit goes to Nate Stein for helping me a lot with the plotting
+        INCHES_PER_ROW, INCHES_PER_COL = 2, 2
+        
+        rows = math.floor(len(img_arrays)/ncols)
+        if len(img_arrays)%ncols!=0:
+            rows= min_rows + 1
+            
+        fig_width = ncols*INCHES_PER_COL
+        fig_height = rows*INCHES_PER_ROW
+        
+        
+        f, ax = plt.subplots(rows, ncols, figsize=(fig_width, fig_height),
+                             sharex=True, sharey=True)
+
+        # Indices to plot in correct axis.
+        row = 0
+        col = 0
+
+        if not plt_titles:
+            plt_titles = [None]*len(img_arrays)
+
+        for i, img in enumerate(img_arrays):
+            ax[row, col].imshow(img_arrays[i], cmap='Greys_r')
+            if plt_titles[i]:
+                ax[row, col].set_title(plt_titles[i])
+
+            # Increment axis index.
+            if ((col + 1)%ncols)==0:
+                # Move to next row since we're at last column.
+                col = 0
+                row += 1
+            else:
+                col += 1
+
+        if fig_title:
+            f.suptitle(fig_title)
+        if save_path:
+            plt.savefig(save_path, bbox_inches='tight')
+        if show_img:
+            plt.show()
+        else:
+            plt.close(f)
+            
+    def get_mean_images (self):
+        return self.centroids
+
+    
+    def create_image_from_array (img_array, show_img=False):
+
+        f = plt.figure()
+        plt.imshow(img_array, cmap='Greys_r')
+        plt.show()
+        return
+            
 
 # This line loads the images for you. Don't change it! 
-pics = np.load("images.npy", allow_pickle=False)
+images = np.load("images.npy", allow_pickle=False)
+print("shape of images: ",images.shape)
+images = images.reshape((images.shape[0], images.shape[1] * images.shape[2])) 
+print("shape of images: ",images.shape)
 
-# You are welcome to change anything below this line. This is just an example of how your code may look.
-# That being said, keep in mind that you should not change the constructor for the KMeans class, 
-# though you may add more public methods for things like the visualization if you want.
-# Also, you must cluster all of the images in the provided dataset, so your code should be fast enough to do that.
+# K-means from couple random restarts and 
+# various values of K.
+
+results = pd.DataFrame(columns=['random_restart', 'K', 'inertia'])
+
+for K in [6, 9, 12]:
+    for random_seed in [69,96]:
+        
+    
+        k_means = KMeans(K, rand_seed=random_seed)
+        k_means.fit(images)
+        
+        distortion = k_means.get_distortion()
+        
+        # create additional df row
+        new_row = {'random_restart': random_seed, 'K': K, 'distortion': distortion}
+        
+        results = results.append(new_row, ignore_index=True)
+        
+        results.loc[:,'random_restart'] =results.loc[:,'random_restart'].astype(int)
+        
+        # Save centroids' images.
+
+        # Credit goes to Nate Stein for helping me a lot with the plotting code!
+
+        # File path designates: (i) K used to fit model (ii) random seed.
+        fig_title = 'Centroids | K = {}, rand = {}'.format(K, random_seed)
+        centr_save_path = 'plots/centr_{}_{}.png'.format(K, random_seed)
+        plot_titles = ['Cluster {}'.format(i + 1) for i in range(K)]
+
+        centroid_imgs = [img.reshape(28, -1) for img in k_means.centroids]
+        k_means.get_plots(centroid_imgs, ncols=3, show_img=False,
+                     save_path=centr_save_path, fig_title=fig_title,
+                     plt_titles=plot_titles)
+
+        # Create representative image subplot titles.
+        repr_imgs = k_means.get_representative_images(3, as_dict=False)
+        repr_imgs = [img.reshape(28, -1) for img in repr_imgs]
+        plot_titles = []
+        for k in range(K):
+            for _ in range(3):
+                plot_titles.append('Cluster {}'.format(k + 1))
+
+        # Save representative images.
+        repr_path = 'plots/repr_{}_{}.png'.format(K, random_seed)
+        fig_title = 'Representative Images | K = {}, rand = {}'.format(K, random_seed)
+        k_means.get_plots(repr_imgs, ncols=3, show_img=False,
+                     save_path=repr_path, fig_title=fig_title,
+                     plt_titles=plot_titles)
+        
+# Compare distortion results to sklearn implementation.
+
 K = 10
-KMeansClassifier = KMeans(K=10, useKMeansPP=False)
-KMeansClassifier.fit(pics)
-KMeansClassifier.create_image_from_array(pics[1])
+filip_km = KMeans(K, rand_seed=1)
+filip_km.fit(images)
+out_the_box = cluster.KMeans(K, random_state=1)
+out_the_box.fit(images)
+
+print('The distortion difference from the sklearn implementation: '
+     '{:.2%}'.format(out_the_box.inertia_/filip_km.get_distortion()-1))
+
+
+plt.plot(filip_km.get_distortion(),color='orange')
+plt.xlabel('Epoch')
+plt.ylabel('Distortion')
+
+plt.show()
 
 
 
